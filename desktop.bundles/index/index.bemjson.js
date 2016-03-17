2cb5e97deb95480dbf40da9d51d84fdda97135e0
Vassily Krasnov <mykrasnov@gmail.com>
2016-03-17 13:03:54 +0300

    Исправить путь к фавиконке для продакшена

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,7 +1,7 @@
 module.exports = {
     block : 'page',
     title : 'Title of the page',
-    favicon : '/favicon.ico',
+    favicon : 'favicon.ico',
     head : [
         { elem : 'meta', attrs : { name : 'description', content : '' } },
         { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
@@ -50,6 +50,15 @@ module.exports = {
                     ]
                 }
             ]
-        }
+        },
+        // {
+        //     block : 'row',
+        //     content : [
+        //         {
+        //             block : 'description',
+        //             content : 'text'
+        //         }
+        //     ]
+        // }
     ]
 };
